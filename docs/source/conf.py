2968bf004eaf755f5cff844e1dc6ec00fe1e18bf
Vault108 <44564111+Vault108@users.noreply.github.com>
2024-02-17 02:20:04 -0500

    Update conf.py on-behalf-of:@TownOfGrimlocke <name@organization.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,8 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Grimlocke'
+author = 'Grimlocke SL'
 
 release = '0.1'
 version = '0.1.0'
